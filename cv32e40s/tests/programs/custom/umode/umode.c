0aa322b1a48a82aa3368f256fbe9bd69313696a0
Robin Pedersen <Robin.Pedersen@silabs.com>
2022-06-30 15:47:05 +0200

    umode: setup pmp region to exec in umode
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40s/tests/programs/custom/umode/umode.c
+++ cv32e40s/tests/programs/custom/umode/umode.c
@@ -6,7 +6,22 @@
 
 
 extern volatile int  myfunc(void);
-extern volatile void run_in_umode(int (*funptr)(void));
+//extern volatile void run_in_umode(int (*funptr)(void));
+extern volatile int  run_in_umode(int (*funptr)(void));
+extern volatile void setup_pmp(void);
+extern volatile void set_mmode(void);
+
+
+void u_sw_irq_handler(void) {
+  uint32_t mcause;
+
+  printf("u_sw_irq_handler\n");
+
+  __asm__ volatile("csrrw %0, mcause, x0" : "=r"(mcause));
+  printf("mcause = %d\n", mcause);
+
+  exit(EXIT_FAILURE);
+}
 
 
 int main(void) {
@@ -17,10 +32,18 @@ int main(void) {
   __asm__ volatile("nop");
   __asm__ volatile("nop");
 
+  x = 0;
+  printf("x = %d\n", x);
   x = myfunc();
   printf("x = %d\n", x);
 
-  run_in_umode(myfunc);
+  setup_pmp();
+
+  x = 0;
+  printf("x = %d\n", x);
+  x = run_in_umode(myfunc);
+  set_mmode();
+  printf("x = %d\n", x);
 
   // Easily visible footer
   __asm__ volatile("nop");
