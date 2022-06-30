2d2696cabd92811f50f557d5e5761db4e1ed3e74
Robin Pedersen <Robin.Pedersen@silabs.com>
2022-06-30 16:48:13 +0200

    umode: request return to mmode after func completes
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40s/tests/programs/custom/umode/umode.c
+++ cv32e40s/tests/programs/custom/umode/umode.c
@@ -11,14 +11,35 @@ extern volatile int  run_in_umode(int (*funptr)(void));
 extern volatile void setup_pmp(void);
 extern volatile void set_mmode(void);
 
+extern volatile uint32_t gbl_request_mmode = 0;
 
+
+__attribute__ ((interrupt ("machine")))
 void u_sw_irq_handler(void) {
   uint32_t mcause;
+  uint32_t mepc;
+  uint32_t mstatus;
 
   printf("u_sw_irq_handler\n");
 
   __asm__ volatile("csrrw %0, mcause, x0" : "=r"(mcause));
   printf("mcause = %d\n", mcause);
+
+  if (gbl_request_mmode) {
+    gbl_request_mmode = 0;
+
+    // Increment "mepc"
+    __asm__ volatile("csrrw %0, mepc, x0" : "=r"(mepc));
+    mepc += 4;
+    __asm__ volatile("csrrw x0, mepc, %0" : : "r"(mepc));
+
+    // Set mmode again
+    __asm__ volatile("csrrw %0, mstatus, x0" : "=r"(mstatus));
+    mstatus |= (3 << 11);
+    __asm__ volatile("csrrw x0, mstatus, %0" : : "r"(mstatus));
+
+    return;
+  }
 
   exit(EXIT_FAILURE);
 }
@@ -42,7 +63,6 @@ int main(void) {
   x = 0;
   printf("x = %d\n", x);
   x = run_in_umode(myfunc);
-  set_mmode();
   printf("x = %d\n", x);
 
   // Easily visible footer
